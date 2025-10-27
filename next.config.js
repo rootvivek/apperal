0615a75617300749953904860bc029f9849e1f39
rootvivek <rootvivek@gmail.com>
2025-10-28 00:28:54 +0530

    Ready for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 // See https://nextjs.org/docs/app/api-reference/config/next-config-js
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    domains: ['ugzyijiuhchxbuiooclv.supabase.co'],
+    unoptimized: true, // Disable image optimization for Netlify
+  },
+}
 
 module.exports = nextConfig
